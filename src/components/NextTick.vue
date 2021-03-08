fa85f8df6a8126be9491ef17cbf8ccfd0f736ecc
shiheng2016 <13120959887@163.com>
2021-03-08 11:44:30 +0800

    vue nextTick 原理

---
+++ src/components/NextTick.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nextTick',
+  data() {
+    return {
+      msg: 'world'
+    }
+  },
+  mounted () {
+    // 作为一个 Promise 使用 (2.1.0 起新增，详见接下来的提示)
+    Vue.nextTick().then(function () {
+        // DOM 更新了
+      })
+    }
+}
+</script>
---
+++ src/public/nextTick.js
@@ -0,0 +1,61 @@
+// The nextTick behavior leverages the microtask queue, which can be accessed
+// via either native Promise.then or MutationObserver.
+// MutationObserver has wider support, however it is seriously bugged in
+// UIWebView in iOS >= 9.3.3 when triggered in touch event handlers. It
+// completely stops working after triggering a few times... so, if native
+// Promise is available, we will use it:
+/* istanbul ignore next, $flow-disable-line */
+// JavaScript 运行机制（Event Loop）
+
+// 如果支持 Promise 就用 Promise
+if (typeof Promise !== "undefined" && isNative(Promise)) {
+  const p = Promise.resolve();
+  timerFunc = () => {
+    p.then(flushCallbacks);
+    // In problematic UIWebViews, Promise.then doesn't completely break, but
+    // it can get stuck in a weird state where callbacks are pushed into the
+    // microtask queue but the queue isn't being flushed, until the browser
+    // needs to do some other work, e.g. handle a timer. Therefore we can
+    // "force" the microtask queue to be flushed by adding an empty timer.
+    if (isIOS) setTimeout(noop);
+  };
+  isUsingMicroTask = true;
+
+} else if (
+  //如果不支持就用 MutationObserver  它会在指定的DOM发生变化时被调用
+  !isIE &&
+  typeof MutationObserver !== "undefined" &&
+  (isNative(MutationObserver) ||
+    // PhantomJS and iOS 7.x
+    MutationObserver.toString() === "[object MutationObserverConstructor]")
+) {
+  // Use MutationObserver where native Promise is not available,
+  // e.g. PhantomJS, iOS7, Android 4.4
+  // (#6466 MutationObserver is unreliable in IE11)
+  let counter = 1;
+  const observer = new MutationObserver(flushCallbacks);
+  const textNode = document.createTextNode(String(counter));
+  observer.observe(textNode, {
+    characterData: true
+  });
+  timerFunc = () => {
+    counter = (counter + 1) % 2;
+    textNode.data = String(counter);
+  };
+  isUsingMicroTask = true;
+  
+  // 如果不支持 MutationObserver 的话就用 setImmediate
+} else if (typeof setImmediate !== "undefined" && isNative(setImmediate)) {
+  // Fallback to setImmediate.
+  // Technically it leverages the (macro) task queue,
+  // but it is still a better choice than setTimeout.
+  timerFunc = () => {
+    setImmediate(flushCallbacks);
+  };
+} else {
+  // Fallback to setTimeout.
+  // 最后一个条件 如果这些都不支持的话就用setTimeout。
+  timerFunc = () => {
+    setTimeout(flushCallbacks, 0);
+  };
+}
